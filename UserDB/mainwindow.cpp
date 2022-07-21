d9378c748cd4ef631da46f3db5c457c86893451b
Aidan <36462380+bigdog765@users.noreply.github.com>
2022-07-21 13:16:35 -0400

    queries

--- UserDB/mainwindow.cpp
+++ UserDB/mainwindow.cpp
@@ -3,6 +3,7 @@
 #include <QtSql>
 #include <QSqlDatabase>
 #include <QMessageBox>
+#include <QSqlQuery>
 
 
 
@@ -19,24 +20,45 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::uploadUser(QString first, QString last, int age){
-    ui->attempt->setText("Attempting to Connect...");
-    if(initDatabase()){
+    ui->attempt->setText("Connecting to Database...");
+    bool connected = false;
+    connected = initDatabase();
+    if(connected){
+        ui->attempt->setText("Connected");
+        QString date = QDate::currentDate().toString();
         //START SQL QUERIES
+
+        QSqlQuery query;
+            query.prepare("INSERT INTO Users (userID, first, last, age, dateOfActivation) "
+                          "VALUES (?, ?, ?, ?, ?)");
+
+            query.bindValue(1, first);
+            query.bindValue(2, last);
+            query.bindValue(3, age);
+            query.bindValue(4, date);
+            if(query.exec()){
+                qDebug() << "Query Done";
+            }else{
+
+                qDebug() << "Query not executed";
+                showError(query.lastError());
+            }
+
     }
+    else ui->attempt->setText("Error Connecting to Database");
 }
 
 bool MainWindow::initDatabase()
 {
     const QString ADDRESS = "34.71.81.194"; //GOOGle cloud IP address
     const QString USERNAME = "root";
-    const QString PASSWORD = "Hf~GHaPFppN3U'x/";
+    const QString PASSWORD = ""; //password not working
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    //db.setDatabaseName("User");
+    db.setDatabaseName("demo-db");
     db.setHostName(ADDRESS);
     db.setUserName(USERNAME);
     db.setPassword(PASSWORD);
-
 
 
     if (db.open()){
